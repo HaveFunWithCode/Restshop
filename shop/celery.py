d2cec06cba22275338bd5ded2e40cbbaa74a2643
marzieh <marziehsepehr@gmail.com>
2020-03-25 15:42:37 +0430

    add celery setting

--- shop/celery.py
+++ shop/celery.py
@@ -4,22 +4,14 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celeryrev.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop.settings')
 app = Celery('celeryconfig')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
-app.conf.beat_schedule = {
-    'hello_hasan': {
-        'task': 'jam zadan',
-        'schedule': 2.0,
-        'args': (15, 16)
-    },
-}
 
 
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
-
 
