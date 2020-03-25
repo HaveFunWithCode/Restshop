4c9e9a3f9d6e9d8edea15b40a77333013e1fa4bf
marzieh <marziehsepehr@gmail.com>
2020-03-25 15:42:06 +0430

    add order to admin for change shippinf status

--- shipping/admin.py
+++ shipping/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['shipping_status','created_at','customer']
+    list_filter=['shipping_status']
+    ordering = ['created_at']
---
+++ shop/celery.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celeryrev.settings')
+app = Celery('celeryconfig')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'hello_hasan': {
+        'task': 'jam zadan',
+        'schedule': 2.0,
+        'args': (15, 16)
+    },
+}
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+
